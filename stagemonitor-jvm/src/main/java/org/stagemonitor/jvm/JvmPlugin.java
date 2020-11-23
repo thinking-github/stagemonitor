30a23e6c97fc928bf11687f1800d3121c31469a0
chenyicheng <chenyicheng00@gmail.com>
2020-11-23 16:03:34 +0800

    JvmPlugin  add jvm_threads[count,peakCount,daemonCount,totalStartedCount] and jvm_classes[loadedCount,totalLoadedCount,unloadedCount]  metrics

--- stagemonitor-jvm/src/main/java/org/stagemonitor/jvm/JvmPlugin.java
+++ stagemonitor-jvm/src/main/java/org/stagemonitor/jvm/JvmPlugin.java
@@ -2,6 +2,7 @@ package org.stagemonitor.jvm;
 
 
 import com.codahale.metrics.Gauge;
+import com.codahale.metrics.jvm.BufferPoolMetricSet;
 import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
 import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 
@@ -9,14 +10,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.stagemonitor.core.CorePlugin;
 import org.stagemonitor.core.StagemonitorPlugin;
+import org.stagemonitor.core.metrics.metrics2.Metric2Registry;
 import org.stagemonitor.core.metrics.metrics2.Metric2Set;
 import org.stagemonitor.core.metrics.metrics2.MetricName;
 import org.stagemonitor.core.metrics.metrics2.MetricNameConverter;
+
+import javax.management.MBeanServer;
+import java.lang.management.ManagementFactory;
 
 import static org.stagemonitor.core.metrics.metrics2.MetricName.name;
 
 public class JvmPlugin extends StagemonitorPlugin {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	private static final MBeanServer server = ManagementFactory.getPlatformMBeanServer();
 
 	@Override
 	public void initializePlugin(StagemonitorPlugin.InitArguments initArguments) {
@@ -37,6 +44,63 @@ public class JvmPlugin extends StagemonitorPlugin {
 				return name("jvm_memory_" + split[0].replace('-', '_')).type(split[1]).build();
 			}
 		}));
+
+		// add nio memory BufferPool  java.nio:type=BufferPool
+		initArguments.getMetricRegistry().registerAll(Metric2Set.Converter.convert(new BufferPoolMetricSet(server), new MetricNameConverter() {
+			@Override
+			public MetricName convert(String name) {
+				final String[] split = name.split("\\.");
+				return name("nio_bufferPool_" + split[0].replace('-', '_')).type(split[1]).build();
+			}
+		}));
+
+		//add JVM thread
+		Metric2Registry registry = initArguments.getMetricRegistry();
+		registry.register(name("jvm_threads").type("count").build(), new Gauge<Integer>() {
+			@Override
+			public Integer getValue() {
+				return ManagementFactory.getThreadMXBean().getThreadCount();
+			}
+		});
+		registry.register(name("jvm_threads").type("peakCount").build(), new Gauge<Integer>() {
+			@Override
+			public Integer getValue() {
+				return ManagementFactory.getThreadMXBean().getPeakThreadCount();
+			}
+		});
+		registry.register(name("jvm_threads").type("totalStartedCount").build(), new Gauge<Long>() {
+			@Override
+			public Long getValue() {
+				return ManagementFactory.getThreadMXBean().getTotalStartedThreadCount();
+			}
+		});
+		registry.register(name("jvm_threads").type("daemonCount").build(), new Gauge<Integer>() {
+			@Override
+			public Integer getValue() {
+				return ManagementFactory.getThreadMXBean().getDaemonThreadCount();
+			}
+		});
+
+		//add JVM class loaded
+		registry.register(name("jvm_classes").type("loadedCount").build(), new Gauge<Integer>() {
+			@Override
+			public Integer getValue() {
+				return ManagementFactory.getClassLoadingMXBean().getLoadedClassCount();
+			}
+		});
+		registry.register(name("jvm_classes").type("totalLoadedCount").build(), new Gauge<Long>() {
+			@Override
+			public Long getValue() {
+				return ManagementFactory.getClassLoadingMXBean().getTotalLoadedClassCount();
+			}
+		});
+		registry.register(name("jvm_classes").type("unloadedCount").build(), new Gauge<Long>() {
+			@Override
+			public Long getValue() {
+				return ManagementFactory.getClassLoadingMXBean().getUnloadedClassCount();
+			}
+		});
+
 
 		final CpuUtilisationWatch cpuWatch;
 		try {
